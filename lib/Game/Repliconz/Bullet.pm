84465294a89b9bab5085fd5a1bec476358d377b0
John Barrett <johna.barrett@gmail.com>
2014-01-24 17:54:31 +0000

    Squaring always gives us positive number, no need for abs.

--- lib/Game/Repliconz/Bullet.pm
+++ lib/Game/Repliconz/Bullet.pm
@@ -16,7 +16,7 @@ sub new {
     # normalise vector : guy position -> target position
     $opts->{v_y} = $opts->{target_y} - $opts->{y};
     $opts->{v_x} = $opts->{target_x} - $opts->{x};
-    my $v_len = sqrt(abs( $opts->{v_y} ** 2 + $opts->{v_x} ** 2 ));
+    my $v_len = sqrt($opts->{v_y} ** 2 + $opts->{v_x} ** 2);
     $opts->{v_y} /= $v_len;
     $opts->{v_x} /= $v_len;
 
